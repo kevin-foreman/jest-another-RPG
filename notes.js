063157fb92fa061456fb49ff57a17cb3223229a7
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-05 15:00:12 -0500

    Added player mock data and made Potion constructor available to Player constructor

---
+++ notes.js
@@ -0,0 +1,12 @@
+
+/*    Clean Code S.O.L.I.D    */
+
+// **Single Responsibility Principle**
+
+// **Open/Closed Princiiple**
+
+// **Liskov Substitution Principle**
+
+// **Interface Segregation Principle**
+
+// **Dependency Inversion Principle**
